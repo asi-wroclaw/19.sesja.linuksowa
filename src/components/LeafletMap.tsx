f58d123ce79415f44410267b90b0636b65a7729c
Michał Korczak <michal.k2001@gmail.com>
2025-01-02 09:12:41 +0000

    fix: missing packages

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -1,19 +1,18 @@
+import type { LatLngTuple } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 const LeafletMap = () => {
-  const position = [51.110424, 17.059968];
+  const position: LatLngTuple = [51.110424, 17.059968];
 
   return (
     <MapContainer
-      //@ts-ignore
       center={position}
       zoom={17}
       scrollWheelZoom={false}
       style={{ height: 450, width: '100%', maxWidth: 600 }}
     >
       <TileLayer
-        //@ts-ignore
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
