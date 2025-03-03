9ca92028cdf45a0203d264aaf18634a6aa3c334c
Felix <110098604+emo-mruczek@users.noreply.github.com>
2025-03-03 21:20:11 +0100

    Slomka (#11)
    * add: slomka
    
    * add: skrzypczyk

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -5,20 +5,12 @@ import Zielinski from '@/assets/speakers/zielinski.webp';
 import Krysinski from '@/assets/speakers/krysinski.webp';
 import Koscik from '@/assets/speakers/koscik.webp';
 import Drzewiecka from '@/assets/speakers/drzewiecka.webp';
+import Slomka from '@/assets/speakers/slomka.webp';
 import Skrzypczyk from '@/assets/speakers/skrzypczyk.webp';
 import type { SpeakerProps } from '@/components/speakers/types';
 
 export const SPEAKERS: SpeakerProps[] = [
-  {
-    name: 'Patryk Kościk, Antmicro',
-    image: Koscik,
-    urls: {},
-    description: [
-      'Patryk Kościk is a Software Engineer at Antmicro, where he has been working for the last three years, focusing on developing and extending the capabilities of the Renode open source simulation framework.',
-      'His expertise lies in understanding systems at the lowest level of abstraction, and he is particularly interested in low-level software development.',
-    ],
-  },
-  {
+   {
     name: 'Tomasz Zieliński',
     image: Zielinski,
     urls: {
@@ -29,6 +21,43 @@ export const SPEAKERS: SpeakerProps[] = [
     },
     description: [
       'Zawodowy programista od 2003 roku, pasjonat bezpieczeństwa informatycznego, autor bloga Informatyk Zakładowy. Rozwijał systemy finansowe dla NBP, tworzył i weryfikował zabezpieczenia bankowych aplikacji mobilnych, brał udział w pracach nad wyszukiwarką internetową Microsoft Bing. Obecnie pracuje w firmie DeepL',
+    ],
+  },
+  {
+    name: 'Piotr (ViruS) Sikora',
+    image: Sikora,
+    urls: {
+      website: 'https://www.linkedin.com/in/piotr-sikora-37a1096/',
+      github: 'https://github.com/piotr-sikora-v',
+      mastodon: 'https://pol.social/@piotrsikora',
+      linkedin: 'https://www.linkedin.com/in/piotr-sikora-37a1096/',
+    },
+    description: [
+      'Z branżą IT związany od ponad 20 lat, specjalizuje się w administracji systemami Linux i budowie rozbudowanych klastrów. Obecnie koncentruje się na innowacjach z zakresu sztucznej inteligencji. Jest fundatorem Fundacji Technologie dla Ludzi, która promuje dostęp do nowoczesnych rozwiązań dla społeczności. Łączy wieloletnią wiedzę techniczną z pasją do innowacji, aby technologia realnie służyła ludziom.',
+    ],
+  },
+  {
+    name: 'Patryk Kościk, Antmicro',
+    image: Koscik,
+    urls: {},
+    description: [
+      'Patryk Kościk is a Software Engineer at Antmicro, where he has been working for the last three years, focusing on developing and extending the capabilities of the Renode open source simulation framework.',
+      'His expertise lies in understanding systems at the lowest level of abstraction, and he is particularly interested in low-level software development.',
+    ],
+  },
+  {
+    name: 'Sebastian Słomka, Nokia',
+    image: Slomka,
+    urls: {
+        linkedin: 'www.linkedin.com/in/sebastian-słomka',
+        github: 'https://github.com/sebastian-slomka',
+    },
+    description: [
+      'Hi, I\'m Sebastian.',
+      'I\'m a passionate software engineer with a deep love for Linux, especially embedded Linux. My journey started early—I installed my first Ubuntu when I was just nine years old, and ever since, I\'ve been fascinated by system architecture and designing complete embedded solutions.',
+      'I\'m also a huge Yocto Project enthusiast and a deep user, both in my work and personal projects. I\'ve been working at Nokia since 2019 as a Technical Leader, where I design and develop in-house embedded Linux distributions for cutting-edge systems.',
+      'Beyond that, I\'m an open-source software enthusiast and an embedded programmer, always striving to build robust, scalable, and secure embedded platforms that stand the test of time.',
+      'When I\'m not immersed in code, I explore the depths as a scuba diver, embracing the challenge and adventure beneath the surface.',
     ],
   },
   {
@@ -45,20 +74,7 @@ export const SPEAKERS: SpeakerProps[] = [
       'Ukończyłem na pierwszym stopniu Politechnikę Wrocławską (Mechatronikę) a studia magisterskie na Uniwersytecie Aalborg w Kopenhagdze (kierunek IT oraz Przedsiębiorczość). Oprócz pracy tworze materiały edukacyjne dot. DevOps czy Linuxa na kanale NetCoffee',
       'Mam doświadczenie w prowadzeniu prezentacji, m.in. dla firmy Akamai. Kilka z moich wystąpień dostępnych jest online na kanale: https://www.youtube.com/@Become-DevOps',
     ],
-  },
-  {
-    name: 'Piotr (ViruS) Sikora',
-    image: Sikora,
-    urls: {
-      website: 'https://www.linkedin.com/in/piotr-sikora-37a1096/',
-      github: 'https://github.com/piotr-sikora-v',
-      mastodon: 'https://pol.social/@piotrsikora',
-      linkedin: 'https://www.linkedin.com/in/piotr-sikora-37a1096/',
-    },
-    description: [
-      'Z branżą IT związany od ponad 20 lat, specjalizuje się w administracji systemami Linux i budowie rozbudowanych klastrów. Obecnie koncentruje się na innowacjach z zakresu sztucznej inteligencji. Jest fundatorem Fundacji Technologie dla Ludzi, która promuje dostęp do nowoczesnych rozwiązań dla społeczności. Łączy wieloletnią wiedzę techniczną z pasją do innowacji, aby technologia realnie służyła ludziom.',
-    ],
-  },
+  }, 
   {
     name: 'Karol Przybylski',
     image: Przybylski,
