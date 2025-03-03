c5f2ecbad0136ddd9a6ff421986f1ed908586b40
Felix <110098604+emo-mruczek@users.noreply.github.com>
2025-03-03 23:36:52 +0100

    Elastic (#13)
    * upd: drzewieckas
    
    * upd: izydoreks site
    
    * add: lasyk
    
    * fix: links
    
    * add: lasyks speech description
    
    * add: lasyk

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -5,6 +5,7 @@ import Zielinski from '@/assets/speakers/zielinski.webp';
 import Krysinski from '@/assets/speakers/krysinski.webp';
 import Koscik from '@/assets/speakers/koscik.webp';
 import Drzewiecka from '@/assets/speakers/drzewiecka.webp';
+import Lasyk from '@/assets/speakers/lasyk.webp';
 import Slomka from '@/assets/speakers/slomka.webp';
 import Skrzypczyk from '@/assets/speakers/skrzypczyk.webp';
 import type { SpeakerProps } from '@/components/speakers/types';
@@ -59,6 +60,43 @@ export const SPEAKERS: SpeakerProps[] = [
       'When I\'m not immersed in code, I explore the depths as a scuba diver, embracing the challenge and adventure beneath the surface.',
     ],
   },
+  {
+    name: 'Szymon Izydorek',
+    image: Izydorek,
+    urls: {
+      youtube: 'https://www.youtube.com/@Become-DevOps',
+      medium: 'https://medium.com/@becomedevops1',
+      github: 'https://github.com/szymonizydorek/',
+      linkedin: 'https://www.linkedin.com/in/sizydorek/',
+    },
+    description: [
+      'Jestem inżynierem DevOps z pasją do zarządzania infrastrukturą IT oraz automatyzacji procesów. Specjalizuję się w pracy z systemami Linux oraz narzędziem Ansible, co pozwala mi efektywnie wdrażać rozwiązania DevOps i zarządzać środowiskami on-prem.',
+      'Ukończyłem na pierwszym stopniu Politechnikę Wrocławską (Mechatronikę) a studia magisterskie na Uniwersytecie Aalborg w Kopenhagdze (kierunek IT oraz Przedsiębiorczość). Oprócz pracy tworze materiały edukacyjne dot. DevOps czy Linuxa na kanale NetCoffee',
+      'Mam doświadczenie w prowadzeniu prezentacji, m.in. dla firmy Akamai. Kilka z moich wystąpień dostępnych jest online na kanale: https://www.youtube.com/@Become-DevOps',
+    ],
+  }, 
+  {
+    name: 'Maciej Lasyk, Elastic',
+    image: Lasyk,
+    urls: {
+        website: 'https://maciej.lasyk.info',
+        github: 'https://github.com/docent-net',
+        mastodon: 'https://n3trunn3r.org/@docent',
+        linkedin: 'https://pl.linkedin.com/in/maciej-lasyk',
+    },
+    description: [
+      'SRE w Elasticu. W wolnym czasie rozwija kilka projektów open - source\'owych (patrz Github). Serce w dużej części oddał Pythonowi (choć obecnie pracuje głównie w Go) oraz Fedorze. Od wielu lat jest w skomplikowanym związku z systemd oraz Selinuxem. Uwielbia hackathony, restartować serwery, których inni się boją dotykać i automatyzuje wszystko co ma sens automatyzować. Na co dzień pracuje we wszystkich popularnych chmurach, ale najlepiej odnajduje się w tej, którą stworzył u siebie w piwnicy. Żyje sportami wytrzymałościowymi, fotografią oraz modelarstwem. Woli biegówki niż zwykłe narty, pływanie z dala od brzegu oraz spoglądać za horyzont, a nie tylko przed siebie.',
+    ],
+  },
+  {
+    name: 'Patryk Kościk, Antmicro',
+    image: Koscik,
+    urls: {},
+    description: [
+      'Patryk Kościk is a Software Engineer at Antmicro, where he has been working for the last three years, focusing on developing and extending the capabilities of the Renode open source simulation framework.',
+      'His expertise lies in understanding systems at the lowest level of abstraction, and he is particularly interested in low-level software development.',
+    ],
+  }, 
   {
     name: 'Szymon Izydorek',
     image: Izydorek,
