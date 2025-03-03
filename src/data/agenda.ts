1ea060c46224047ffcf6809e0d02e3508faa6d98
emo-mruczek <krokcia1@gmail.com>
2025-03-03 21:15:22 +0100

    add: skrzypczyk

--- src/data/agenda.ts
+++ src/data/agenda.ts
@@ -107,6 +107,17 @@ export const SATURDAY_AGENDA: SpeechItem[] = [
   {
     start: 'TBA',
     end: 'TBA',
+    title: 'Beyond Raspbian and Ubuntu: Why Yocto is the Right Tool for Production-Ready Embedded Linux',
+    author: 'Sebastian Słomka',
+    lang: ['en'],
+    description: [
+      'Many developers start their embedded Linux journey using general-purpose distributions like Ubuntu or Raspbian. While these are great for prototyping and development, they fall short when scaling to production-ready, high-performance, and long-term maintainable embedded systems. This talk will explore why Yocto is the preferred tool for professional embedded Linux development and how it addresses key challenges that pre-built distributions cannot.',
+    ],
+    recording: '',
+  },
+  {
+    start: 'TBA',
+    end: 'TBA',
     title: '',
     author: '',
     //lang: [''],
