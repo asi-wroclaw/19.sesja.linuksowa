0a2f378549496c64ada7da4f906830e50d739e55
Michał Korczak <michal.k2001@gmail.com>
2025-03-12 01:01:36 +0000

    feat: wit & sekurak

--- src/data/organizations.ts
+++ src/data/organizations.ts
@@ -1,17 +1,18 @@
+import antmicro from '@/assets/logo/antmicro.svg';
 import asi from '@/assets/logo/asi-logo-trans.svg';
 import clug from '@/assets/logo/clug.png';
 import ftdl from '@/assets/logo/ftdl.webp';
-import hswro from '@/assets/logo/hswro.svg';
-import manus from '@/assets/logo/manus.png';
-import plug from '@/assets/logo/plugo-cienie-url.svg';
-import pwr from '@/assets/logo/pwr.png';
-import antmicro from '@/assets/logo/antmicro.svg';
 import hspoz from '@/assets/logo/knyfyrtel_logo_nocircle_lightbg.webp';
-import nokia from '@/assets/logo/nokia.jpg';
-import programistamag from '@/assets/logo/programistamag.jpg';
+import lpi from '@/assets/logo/LPI_Logo-Colour.svg';
+import manus from '@/assets/logo/manus.png';
 import mikrus from '@/assets/logo/mikrus.png';
-import LPI from '@/assets/logo/LPI_Logo-Colour.svg';
-import Zaufana from '@/assets/logo/z3s.png';
+import nokia from '@/assets/logo/nokia.jpg';
+import plug from '@/assets/logo/plugo-cienie-url.svg';
+import programistamag from '@/assets/logo/programistamag.jpg';
+import pwr from '@/assets/logo/pwr.png';
+import sekurak from '@/assets/logo/sekurak1.png';
+import wit from '@/assets/logo/wit.png';
+import zaufana from '@/assets/logo/z3s.png';
 import type { SponsorshipEntity } from '@/components/sponsorship/types';
 
 export const ORGANIZATIONS = [
@@ -33,7 +34,7 @@ export const ORGANIZATIONS = [
     title: 'honorary-patrons',
     logos: [
       { image: pwr, href: 'https://pwr.edu.pl/' },
-      // { image: wit, href: 'https://wit.pwr.edu.pl/' },
+      { image: wit, href: 'https://wit.pwr.edu.pl/' },
     ],
   },
   {
@@ -45,8 +46,9 @@ export const ORGANIZATIONS = [
       { image: mikrus, href: 'https://mikr.us' },
       { image: programistamag, href: 'https://programistamag.pl/'},
       { image: manus, href: 'https://manus.pl/' },
-      { image: LPI, href: 'https://www.lpi.org/' },
-      { image: Zaufana, href: 'https://zaufanatrzeciastrona.pl/' },
+      { image: lpi, href: 'https://www.lpi.org/' },
+      { image: zaufana, href: 'https://zaufanatrzeciastrona.pl/' },
+      { image: sekurak, href: 'https://sekurak.pl/' },
     ],
   },
 ] as SponsorshipEntity[];
