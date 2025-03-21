9f7d641b6cfbda98f242e6d35fc0adb6d689d154
Michał Korczak <michal.k2001@gmail.com>
2025-03-21 01:29:04 +0000

    feat: partners

--- src/data/organizations.ts
+++ src/data/organizations.ts
@@ -2,6 +2,7 @@ import antmicro from '@/assets/logo/antmicro.svg';
 import asi from '@/assets/logo/asi-logo-trans.svg';
 import clug from '@/assets/logo/clug.png';
 import ftdl from '@/assets/logo/ftdl.webp';
+import futureconf from '@/assets/logo/futureconf.svg';
 import hspoz from '@/assets/logo/knyfyrtel_logo_nocircle_lightbg.webp';
 import lpi from '@/assets/logo/LPI_Logo-Colour.svg';
 import manus from '@/assets/logo/manus.png';
@@ -10,6 +11,7 @@ import nokia from '@/assets/logo/nokia.jpg';
 import plug from '@/assets/logo/plugo-cienie-url.svg';
 import programistamag from '@/assets/logo/programistamag.jpg';
 import pwr from '@/assets/logo/pwr.png';
+import reszka from '@/assets/logo/reszka.png';
 import sekurak from '@/assets/logo/sekurak.png';
 import wit from '@/assets/logo/wit.png';
 import zaufana from '@/assets/logo/z3s.png';
@@ -49,6 +51,8 @@ export const ORGANIZATIONS = [
       { image: lpi, href: 'https://www.lpi.org/' },
       { image: zaufana, href: 'https://zaufanatrzeciastrona.pl/' },
       { image: sekurak, href: 'https://sekurak.pl/' },
+      { image: futureconf, href: 'https://futureconf.tech/' },
+      { image: reszka, href: 'https://reszka.org/' },
     ],
   },
 ] as SponsorshipEntity[];
