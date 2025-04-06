4f668796f7108c222fb04c021081247eab47dc24
Michał Korczak <michal.k2001@gmail.com>
2025-04-06 07:45:02 +0000

    feat: sunday

--- src/data/common.ts
+++ src/data/common.ts
@@ -1,6 +1,6 @@
 export const callForPapersUrl = 'https://forms.gle/SjDg435wR9YXMSkY9';
 
-export const liveUrl = 'https://youtube.com/live/X1mP3yqL-IM';
+export const liveUrl = 'https://youtube.com/live/-0vw7621IwM';
 
 export const youtubeVideoId = 'CsCnZ7DKQQ4';
 export const youtubePlaylistId = 'PLTvZBPQxCI8EWxk9lhgOP8wSg7572N5Up';
