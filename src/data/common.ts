de5e2431f4f885137cc3751b81cf884a10f9cbed
Michał Korczak <michal.k2001@gmail.com>
2025-08-10 03:15:18 +0000

    feat: keynote

--- src/data/common.ts
+++ src/data/common.ts
@@ -2,9 +2,10 @@ export const callForPapersUrl = 'https://forms.gle/SjDg435wR9YXMSkY9';
 
 export const liveUrl = 'https://youtube.com/live/-0vw7621IwM';
 
-export const youtubeVideoId = 'CsCnZ7DKQQ4';
+export const youtubeVideoId = 'KoZKrcoX-eA';
 export const youtubePlaylistId = 'PLTvZBPQxCI8EWxk9lhgOP8wSg7572N5Up';
-export const youtubePlaylistUrl = `https://www.youtube.com/watch?v=${youtubeVideoId}&list=${youtubePlaylistId}`;
+// export const youtubePlaylistUrl = `https://www.youtube.com/watch?v=${youtubeVideoId}&list=${youtubePlaylistId}`;
+export const youtubePlaylistUrl = `https://www.youtube.com/watch?v=${youtubeVideoId}`;
 
 export const ticketsUrl =
   'https://app.evenea.pl/event/19-sesja-linuksowa/?source=www-linuksowa';
